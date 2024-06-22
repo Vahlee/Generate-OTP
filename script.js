3f9e0809e54a4c2e95501efa9c2c72c718a3bf19
Violet Ovedje <violetiruoghene@gmail.com>
2024-06-23 00:19:46 +0100

    upload files

---
+++ script.js
@@ -0,0 +1,23 @@
+const otpBox = document.getElementById ("Password");
+const length = 6;
+const number = "0123456789";
+const otpChar = number;
+
+function createOTP(){
+    let password = '';
+    password += number [Math.floor(Math.random()) * number.length];
+
+    while (length > password.length) {
+        password += otpChar [Math.floor(Math.random() * otpChar.length)];
+    }
+    otpBox.value = password;
+    copyButton.addEventListener('click', function() {
+        const text = otpBox.value;
+        navigator.clipboard.writeText(text).then(function() {
+            alert('Text copied to clipboard successfully!');
+        }).catch(function(error) {
+            alert('Error copying text to clipboard: ', error);
+        });
+    });
+
+}
